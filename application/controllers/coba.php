085fa43ad3c52c58b4ecf5cc46c654b2c804b660
septiarrobert <161240000509@unisnu.ac.id>
2019-10-28 22:35:11 +0700

    revisi

---
+++ application/controllers/coba.php
@@ -0,0 +1,17 @@
+<?php 
+class Coba 
+{
+	private $nama;
+	private $status;
+
+	public function __construct($nama, $status)
+	{
+		$this->nama = $nama;
+		$this->status = $status;
+	}
+
+	public function tampil()
+	{
+		$text = 'Perkenalkan Saya '.$this
+	}
+} ?>
